53a810a160d2e797f16eb6a7eabde3c05c906eba
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-21 06:16:52 -0400

    Add files via upload

---
+++ FlightBot.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+
+namespace FlightBot
+{
+    class FlightScraper
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://www.google.com/travel/flights/search?tfs=CBwQAhooagwIAhIIL20vMGZfXzESCjIwMjItMDgtMDZyDAgDEggvbS8wZjJ2MBooagwIAxIIL20vMGYydjASCjIwMjItMDgtMTByDAgCEggvbS8wZl9fMXABggELCP___________wFAAUgBmAEB";
+            
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+
+            Console.WriteLine(html.Result);
+            Console.ReadLine();
+         }
+    }
+}
