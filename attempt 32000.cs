180430475d31725fb8e3e183c065bee56c0f81da
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-22 03:11:46 -0400

    Add files via upload

---
+++ attempt 32000.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace trynumber720000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+        public ChromiumWebBrowser browser;
+
+        public ChromiumWebBrowser webbrowser;
+
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://www.kayak.com/"); 
+            this.Controls.Add(browser);
+
+            browser.Dock = DockStyle.Fill;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser.ExecuteScriptAsync("document.getElementById('keel-container form-container s-t-bp no-edges edges-s').value=" + '\'' +  + '\'');
+            browser.ExecuteScriptAsync("document.getElementById('zEiP-formField zEiP-destination').value=" + '\'' +  + '\'');
+            browser.ExecuteScriptAsync("document.getElementsByName('zEiP-formField zEiP-submit')[0].click()");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
