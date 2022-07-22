6ac256ad7a010a4cc80d3c2ea9ea2484424dc270
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-21 23:03:55 -0400

    Add files via upload

---
+++ FLIGHTBOTUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace FlightBot
+{
+    public partial class FlightBOT : Form
+    {
+        public FlightBOT()
+        {
+            InitializeComponent();
+            WebBrowser.Navigate("https://www.google.com/travel/flights");
+        }
+        private void label1_Click(object sender, EventArgs e) //from**
+        {
+            
+        }
+        private void label2_Click(object sender, EventArgs e) //to**
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) //date1**
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e) //date2**
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
