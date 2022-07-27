448cbebdb6015394bbb46bf88d9856c2cfdeab60
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-27 04:10:33 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,26 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 using HtmlAgilityPack;
 
-
-namespace Ebaybot
+namespace EbayBotTwo
 {
     class Bot
     {
         static void Main(string[] args)
         {
-            HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();
-            HtmlAgilityPack.HtmlDocument document = website.Load("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=yeezy&_sacat=0");
-            var ebayList = document.DocumentNode.SelectNodes("//div[@class ='srp-river-results']").ToList();
-                foreach (var content in ebayList)
-            {
-                Console.WriteLine(content.InnerHtml);
+            Console.ReadLine();
+            HtmlASync();
+        }
+
+        private static async void  HtmlASync()
+        {
+            var url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=yeezy&_sacat=0";
+           
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
           
-            }
-            Console.Read();
+            var ItemList = htmlDoc.DocumentNode.Descendants("ul")
+                .Where(node => node.GetAttributeValue("id", "")
+                .Equals("srp-results__clear")).ToList();
 
+            //var itemListItems = ItemList[0].Descendants("li")
+                //.Where(node => node.GetAttributeValue("id", ""));
+              
+            Console.WriteLine();
+        }
 
-                }
     }
 }
