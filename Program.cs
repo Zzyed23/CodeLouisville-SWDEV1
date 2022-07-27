a94e1a99f1a6d5b91a6e3e7da51431948aaf46e4
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-27 03:18:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+
+namespace Ebaybot
+{
+    class Bot
+    {
+        static void Main(string[] args)
+        {
+            HtmlAgilityPack.HtmlWeb website = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = website.Load("https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=yeezy&_sacat=0");
+            var ebayList = document.DocumentNode.SelectNodes("//div[@class ='srp-river-results']").ToList();
+                foreach (var content in ebayList)
+            {
+                Console.WriteLine(content.InnerHtml);
+          
+            }
+            Console.Read();
+
+
+                }
+    }
+}
