75c19e6ec2b38ad4967bd545868eb72b957a5dc0
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-29 00:57:20 -0400

    Add files via upload

--- csharpps5bot.cs
+++ csharpps5bot.cs
@@ -14,64 +14,64 @@ namespace PS5bot
     class Program
     {
 
-    }
-    static void Main(string[] args)
-    {
-        GetHtmlAsync();
-        Console.ReadLine();
-
-    }
-    private static async void GetHtmlAsync()
-    {
-        //URL that we will parese through
-        var url = "https://www.ebay.com/sch/i.html?LH_SALE_CURRENCY=0&_dmd=1&_ex_kw=&_fpos=40219&_fsct=&_fspt=1&_ftrt=901&_ftrv=1&_in_kw=1&_ipg=60&_nkw=ps5&_sabdhi=&_sabdlo=&_sacat=0&_sadis=10&_samihi=&_samilow=&_sofindtype=5&_sop=12&_udhi=&_udlo=&_fosrp=1";
-
-        var httpCLient = new HttpClient();
-        var html = await httpCLient.GetStringAsync(url);
-
-        var htmlDocument = new HtmlDocument();
-        htmlDocument.LoadHtml(html);
-
-        var ProductsHtml = htmlDocument.DocumentNode.Descendants("ul") //looking for unordered lists
-            .Where(node => node.GetAttributeValue("id", "") //looking for the token "id"
-            .Equals("ListViewInner")).ToList();
-
-        var ProductListItems = ProductsHtml[0].Descendants("li") //looking for the list of products
-            .Where(node => node.GetAttributeValue("id", "") //again another id tag
-            .Contains("item")).ToList()
-
-        foreach (var Product in ProductListItems) // foreach loop to go through the html and display the contents below
+        static void Main(string[] args)
         {
-            //will print the product id
-            Console.WriteLine(ProductListItems.GetAttributeValue("lisitngid", ""));
+            GetHtmlAsync();
+            Console.ReadLine();
 
-            //will print the name of the product
-            Console.WriteLine(ProductListItems.Descendants("h3")
-            .Where(node => node.GetAttributeValue("class", "")
-            .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
-            );
-
-            //will print out the price
-            Console.WriteLine(ProductListItems.Descendants("li")
-            .Where(node => node.GetAttributeValue("class", "")
-            .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
-            );
-
-            //will print out the URL
-            Console.WriteLine(
-                ProductListItems.Descendants("a").FirstOrDefault().GetAtttributeValue("href", ""));
-
-            
-
-            Console.WriteLine();
         }
+        private static async void GetHtmlAsync()
+        {
+            //URL that we will parese through
+            var url = "https://www.ebay.com/sch/i.html?LH_SALE_CURRENCY=0&_dmd=1&_ex_kw=&_fpos=40219&_fsct=&_fspt=1&_ftrt=901&_ftrv=1&_in_kw=1&_ipg=60&_nkw=ps5&_sabdhi=&_sabdlo=&_sacat=0&_sadis=10&_samihi=&_samilow=&_sofindtype=5&_sop=12&_udhi=&_udlo=&_fosrp=1";
 
-        
+            var httpCLient = new HttpClient();
+            var html = await httpCLient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var ProductsHtml = htmlDocument.DocumentNode.Descendants("ul") //looking for unordered lists
+                .Where(node => node.GetAttributeValue("id", "") //looking for the token "id"
+                .Equals("ListViewInner")).ToList();
+
+            var ProductListItems = ProductsHtml[0].Descendants("li") //looking for the list of products
+                .Where(node => node.GetAttributeValue("id", "") //again another id tag
+                .Contains("item")).ToList();
+
+            foreach (var Product in ProductListItems) // foreach loop to go through the html and display the contents below
+            {
+                //will print the product id
+                Console.WriteLine(Product.GetAttributeValue("lisitngid", ""));
+
+                //will print the name of the product
+                Console.WriteLine(Product.Descendants("h3")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
+                );
+
+                //will print out the price
+                Console.WriteLine(Product.Descendants("li")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("lvprice prc")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
+                );
+
+                //will print out the URL
+               // Console.WriteLine(
+                    //Product.Descendants("a").FirstOrDefault().GetAtttributeValue("href", ""));
+
+
+
+                Console.WriteLine();
+            }
+
+
 
 
 
             Console.WriteLine();
 
+        }
     }
 }
 
