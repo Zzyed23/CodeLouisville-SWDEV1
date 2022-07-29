1605495870a777b71ccd2108d0968417d8cd0315
Zzyed23 <102580613+Zzyed23@users.noreply.github.com>
2022-07-29 02:06:44 -0400

    Add files via upload

--- csharpps5bot.cs
+++ csharpps5bot.cs
@@ -21,43 +21,50 @@ namespace PS5bot
 
         }
         private static async void GetHtmlAsync()
-        {
-            //URL that we will parese through
+        {  //URL we will be using to parse. I chose a link for PS5 lisitings
             var url = "https://www.ebay.com/sch/i.html?LH_SALE_CURRENCY=0&_dmd=1&_ex_kw=&_fpos=40219&_fsct=&_fspt=1&_ftrt=901&_ftrv=1&_in_kw=1&_ipg=60&_nkw=ps5&_sabdhi=&_sabdlo=&_sacat=0&_sadis=10&_samihi=&_samilow=&_sofindtype=5&_sop=12&_udhi=&_udlo=&_fosrp=1";
 
-            var httpCLient = new HttpClient();
-            var html = await httpCLient.GetStringAsync(url);
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url); 
 
             var htmlDocument = new HtmlDocument();
             htmlDocument.LoadHtml(html);
 
-            var ProductsHtml = htmlDocument.DocumentNode.Descendants("ul") //looking for unordered lists
-                .Where(node => node.GetAttributeValue("id", "") //looking for the token "id"
-                .Equals("ListViewInner")).ToList();
+            var ProductsHtml = htmlDocument.DocumentNode.Descendants("ul") //ul is a unordered list we will be trying to obtain
+                .Where(node => node.GetAttributeValue("id", "") //looking for the token "id" 
+                .Equals("ListViewInner")).ToList(); 
 
-            var ProductListItems = ProductsHtml[0].Descendants("li") //looking for the list of products
-                .Where(node => node.GetAttributeValue("id", "") //again another id tag
+            var ProductListItems = ProductsHtml[0].Descendants("li")//li is a list of items we want
+                .Where(node => node.GetAttributeValue("id", "")//another id token
                 .Contains("item")).ToList();
 
             Console.WriteLine(ProductListItems.Count() + " Listings");
             Console.WriteLine();
 
-            foreach (var Product in ProductListItems) // foreach loop to go through the html and display the contents below
+            foreach (var Product in ProductListItems)
             {
-                //will print the product id
-                Console.WriteLine(Product.GetAttributeValue("lisitngid", ""));
+                //name of the item
+                Console.WriteLine("Product: " + Product.Descendants("h3")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Contains("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
+                    );
+                //lvtitle is used for the name of the product
 
-                //will print the name of the product
-                Console.WriteLine(Product.Descendants("h3")
-                .Where(node => node.GetAttributeValue("clas2qs", "")
-                .Equals("lvtitle")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
-                );
+                //Price
+                Console.WriteLine("Price: " +
+                        Product.Descendants("li")
+                        .Where(node => node.GetAttributeValue("class", "")
+                        .Contains("lvprice prc")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')  //lvprice is the common marker that we will look for when trying to obtain the price
 
-                //will print out the price
-                Console.WriteLine(Product.Descendants("li")
-                .Where(node => node.GetAttributeValue("class", "")
-                .Equals("lvprice prc")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t')
-                );
+                    );
+
+                //URL direct link for the item
+                Console.WriteLine("URL: " +
+                        Product.Descendants("a")
+                        .Where(node => node.GetAttributeValue("class", "")
+                        .Contains("vip")).FirstOrDefault().GetAttributeValue("href", "")
+                        );
+                //vip is the marker for the url of the item
 
                 Console.WriteLine();
             }
